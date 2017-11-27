29424400091ed2a6bab8747556cca74445f9156e
Roman Chopovenko <[rchopovenko@vinditek.com]>
2017-11-27 22:15:34 +0200

    Initial Commit

---
+++ firest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firest
+//
+//  Created by Roma Chopovenko on 11/27/17.
+//  Copyright © 2017 Roma Chopovenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
